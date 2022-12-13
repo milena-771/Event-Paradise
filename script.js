4c7df3b174ec9231b110aa7096e3cceb4b56e251
MilenaPC <milena771@hotmail.fr>
2022-12-13 13:05:44 +0100

    custom tooltip on invalid field after or before submitted, toaster on left for valid form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 let today = new Date();
 
@@ -24,84 +26,108 @@ const elements = form.elements;
 for (const element of elements){
     const type = element.type;
     const elementName = element.name;
-    if(type != "submit"){
+    if(!element.checkValidity()){
+
         element.addEventListener("invalid", (event) => {
             event.preventDefault();
             
             element.classList.add("is-invalid");
 
             let getElementName = document.getElementById(`${elementName}-text`);
-            getElementName.classList.add("text-danger"); 
-        
-            element.addEventListener("focus", (event) => {
-                let validityState = element.validity;
-                displayTooltip(element, elementName, validityState); 
-            })
-        })
+            getElementName.classList.add("text-danger");
+            
+            let textTooltip = "";
+
+            if(element.validity.valueMissing){
+                textTooltip = "Veuillez remplir ce champs";
+            }else if(elementName == "date" && element.validity.rangeUnderflow){
+                textTooltip = "Veuillez indiquer une date supérieure à aujourd'hui"; 
+            }else if(elementName == "price" && element.validity.rangeUnderflow){
+                textTooltip = "Veuillez indiquer un tarif supérieur à zéro";
+            }
+
+            element.setAttribute("data-bs-toggle", "tooltip");
+            element.setAttribute("data-bs-title", textTooltip);
+            element.setAttribute("data-bs-custom-class", "custom-tooltip");
+            element.setAttribute("data-bs-placement", "top");
+
+            const tooltip = bootstrap.Tooltip.getOrCreateInstance(element);
+
+            let firstInvalidElement = document.querySelector(".is-invalid");
+            
+            firstInvalidElement.focus();
+           
+
+        })    
         element.addEventListener("change", (event) => {
             if(element.checkValidity()){
                 
-                element.classList.replace("is-invalid", "is-valid");
+                element.classList.replace("is-invalid","is-valid");
 
                 let getElementName = document.getElementById(`${elementName}-text`);
                 getElementName.classList.replace("text-danger","text-success"); 
 
+                const tooltip = bootstrap.Tooltip.getOrCreateInstance(element);
+
+                tooltip.dispose();
+
             }else{
-                let validityState = element.validity;
-                displayTooltip(element, elementName, validityState);
+
+                let invalidElement = document.querySelector(".is-invalid");
+
+                let invalidName = invalidElement.name;
+
+                let newTooltip = "";
+
+                if(invalidElement.validity.valueMissing){
+                    newTooltip = "Veuillez remplir ce champs";
+                }else if(invalidName == "date" && invalidElement.validity.rangeUnderflow){
+                    newTooltip = "Veuillez indiquer une date supérieure à aujourd'hui"; 
+                }else if(invalidName == "price" && invalidElement.validity.rangeUnderflow){
+                    newTooltip = "Veuillez indiquer un tarif supérieur à zéro";
+                }
+
+                element.setAttribute("data-bs-toggle", "tooltip");
+                element.setAttribute("data-bs-title", newTooltip);
+                element.setAttribute("data-bs-custom-class", "custom-tooltip");
+                element.setAttribute("data-bs-placement", "top");
+
+
+                const tooltip = bootstrap.Tooltip.getOrCreateInstance(invalidElement);
+
+                invalidElement.focus();
             }
 
         })
-
     }
 }
 
-function displayTooltip(element,elementName, validityState){
-    let textTooltip = "";
 
-    const errorValueMissing = validityState["valueMissing"];
-    const errorRangeUnderFlow = validityState["rangeUnderflow"];
-
-    if(errorValueMissing == true){
-        textTooltip = "Veuillez remplir ce champs";
-    }else if(elementName == "date" && errorRangeUnderFlow  == true){
-        textTooltip = "Veuillez indiquer une date supérieure à aujourd'hui"; 
-    }else if(elementName == "price" && errorRangeUnderFlow  == true){
-        textTooltip = "Veuillez indiquer un tarif supérieur à zéro";
-    }
-
-    element.setAttribute("data-bs-toggle", "tooltip");
-    element.setAttribute("data-bs-title", textTooltip);
-    element.setAttribute("data-bs-custom-class", "custom-tooltip");
-    element.setAttribute("data-bs-placement", "top");
-
-    const tooltip = bootstrap.Tooltip.getOrCreateInstance(element);
-
-    tooltip.show();
-}
-
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-        if(form.reportValidity()){
-            form.reset();
-            const toastBtn = document.getElementById('toastBtn');
-            const myToast= document.getElementById('myToast');
-            displayToast(toastBtn, myToast);
-        }
-    })
-
-function displayToast(toastBtn, myToast){
-    if(toastBtn){
-        toastBtn.addEventListener('click', () =>{
-            const toast = new bootstrap.Toast(myToast);
-            toast.show();
-            console.log(toast.isShown());
-            
-        })
-    }
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+            if(form.reportValidity()){
+                form.reset();
+                displayToaster();       
+            }
+            /*if(element.checkValidity()){
+                const elementName = element.name;
+                element.classList.add("is-valid");
+                let getElementName = document.getElementById(`${elementName}-text`);
+                getElementName.classList.add("text-success");
     
+            }*/
+    })
+    
+
+
+
+//Display a toaster when form is valid
+function displayToaster(){
+    const myToast = document.querySelector('.toast');
+    const toast = bootstrap.Toast.getOrCreateInstance(myToast);
+    toast.show();
 }
+
 
         
 
