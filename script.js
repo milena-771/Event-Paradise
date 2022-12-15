7b033640353b4d9e7c71eec89c6f1d3777dffb79
MilenaPC <milena771@hotmail.fr>
2022-12-15 09:42:12 +0100

    simplified the way to use tooltips

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+let today = new Date(); // create a date Object
 
-let today = new Date();
-
-let day = today.getDate();
-let month = today.getMonth()+1; //January is 0
-let year = today.getFullYear();
+let day = today.getDate(); //assign today's day
+let month = today.getMonth()+1; //January is 0 (assign today's month)
+let year = today.getFullYear();//assign today's year
 
         if(day<10){
                 day='0'+day
@@ -13,57 +10,62 @@ let year = today.getFullYear();
         if(month<10){
             month='0'+month
         }
-        today = year+'-'+month+'-'+ day;
+        today = year+'-'+month+'-'+ day; //Create the correct format for the date
 
-const date = document.getElementById("date");
+const date = document.getElementById("date");//get the input date
 
-date.setAttribute("min", today);
+date.setAttribute("min", today);//Create and set an attribute min starting today
 
+
+//Select my form
 let form = document.querySelector("form");
 
 const elements = form.elements;
 
-for (const element of elements){
-    const type = element.type;
-    const elementName = element.name;
-    if(!element.checkValidity()){
+//Create an option object, to assign default options for our futur tooltips
+const options = {
+    title:"Message par défaut", //title by default otherwise tooltip cannot appear
+    customClass:"custom-tooltip"
+}
 
-        element.addEventListener("invalid", (event) => {
-            event.preventDefault();
+//Loop on elements to isolate the inputs
+for (const element of elements){
+    const elementName = element.name;
+    //Check if one or several inputs are invalids
+    if(!element.checkValidity()){ 
+        element.addEventListener("invalid", (event) => { //Create a callback function when an input is invalid
+            event.preventDefault(); //Disable default beahvior of html
             
+            //add bootstrap classes to make appear an input as invalid
             element.classList.add("is-invalid");
 
             let getElementName = document.getElementById(`${elementName}-text`);
             getElementName.classList.add("text-danger");
 
-            if(element.validity.valueMissing){
-                element.setAttribute("data-bs-title", "Ce champs est obligatoire")
-            }else if(elementName == "date" && element.validity.rangeUnderflow){
-                element.setAttribute("tooltip-inner", "Doit être égale ou supérieure à aujourd'hui");
-            }else if(elementName == "price" && element.validity.rangeUnderflow){
-                element.setAttribute("tooltip-inner", "Doit être positif");
+            let message = "";
+
+            //check if input is invalid because of value Missing and custom the message in the tooltip
+            if(element.validity.valueMissing){ 
+                message = "Ce champs est obligatoire";
+            }else if(elementName == "date" && element.validity.rangeUnderflow){  //check if the date is under the min attribute
+                message = "Doit être égale ou supérieure à aujourd'hui";
+            }else if(elementName == "price" && element.validity.rangeUnderflow){  //check if the price is under 0
+                message = "Doit être positif";
             }
 
-            element.setAttribute("data-bs-toggle", "tooltip");
-            element.setAttribute("data-bs-custom-class", "custom-tooltip");
-            element.setAttribute("data-bs-placement", "top");
-
-            const tooltip = bootstrap.Tooltip.getOrCreateInstance(element);
-
-            if(element.validity.valueMissing){
-                tooltip.setContent({'.tooltip-inner' : 'Ce champs est obligatoire'});
-            }else if(elementName == "date" && element.validity.rangeUnderflow){
-                tooltip.setContent({'.tooltip-inner' : 'Doit être égale ou supérieure à aujourd\'hui'});
-            }else if(elementName == "price" && element.validity.rangeUnderflow){
-                tooltip.setContent({'.tooltip-inner' : 'Doit être positif'});
-            }
-
+            //Create or get a tooltip from an element with specified options
+            const tooltip = bootstrap.Tooltip.getOrCreateInstance(element, options);
+            
+            //modified the message after submitted form if input is still invalid
+            tooltip.setContent({'.tooltip-inner' : message});
+         
             let firstInvalidElement = document.querySelector(".is-invalid");
             
             firstInvalidElement.focus();
            
 
         })    
+        //Create a function when an input is changed by the user
         element.addEventListener("change", (event) => {
             if(element.checkValidity()){
                 
@@ -74,7 +76,7 @@ for (const element of elements){
 
                 const tooltip = bootstrap.Tooltip.getOrCreateInstance(element);
 
-                tooltip.dispose();   
+                tooltip.dispose();//destroy the tooltip
 
             }
 
@@ -83,25 +85,15 @@ for (const element of elements){
 }
 
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-            if(form.reportValidity()){
-                form.reset();
-                displayToaster();       
-            }
-            /*if(element.checkValidity()){
-                const elementName = element.name;
-                element.classList.add("is-valid");
-                let getElementName = document.getElementById(`${elementName}-text`);
-                getElementName.classList.add("text-success");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+        if(form.reportValidity()){//check if all the fields are valid...
+            form.reset();//...the form resets...
+            displayToaster(); //...the function for the toaster is called     
+        }
+})
     
-            }*/
-    })
-    
-
-
-
-//Display a toaster when form is valid
+//Function to display a toaster
 function displayToaster(){
     const myToast = document.querySelector('.toast');
     const toast = bootstrap.Toast.getOrCreateInstance(myToast);
